c9ff90f91202ab6bfdf0a3709401608d17970dd1
Waldo Costa <waldooo@users.noreply.github.com>
2017-12-12 19:25:12 -0200

    first upload

---
+++ LED_control/LED_control.ino
@@ -0,0 +1,44 @@
+class LEDs
+{
+  public:
+    int pino_PWM;
+    float intensidade;
+    float frequencia;
+
+    // construtor (precisa ter o mesmo nome da classe)
+    LEDs(int _pino_PWM, float _frequencia) {
+
+      pino_PWM = _pino_PWM;
+      //pinMode(pino_PWM, OUTPUT);
+      frequencia = _frequencia;
+
+      intensidade = 0;
+
+    }
+
+    // métodos
+    void atualizar() {
+
+      float senoidal = sin(frequencia *2*PI* millis() / 1000);
+
+/*
+ * para que uma onda senoidal tenha seu valor máximo 255 e todos
+ * seus valores positivos, precisamos multiplicar o seno do ângulo por 
+ * 255/2 e somar o valor 255/2 ao valor resultante, assim o valor 
+ * mínimo da onda resultante será 0 e o máximo 255.
+ */
+       intensidade = 127.5 + 127.5 * senoidal;
+      analogWrite(pino_PWM, intensidade);
+    }
+
+    void modificafrequencia(){
+      /*
+       * cada vez que esta função for chamada, o valor da frequência
+       * será aumentado em 1 unidade.
+       */
+      frequencia++;
+    }
+
+};
+
+
---
+++ LED_control/main_loop.ino
@@ -0,0 +1,33 @@
+
+
+unsigned long previousMillis = 0;
+const long interval = 1000; 
+
+
+LEDs meuLED01(5, 0.1);
+LEDs meuLED02(6, 1);
+
+
+void setup() {
+  //Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  meuLED01.atualizar();
+  meuLED02.atualizar();
+
+    unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    meuLED01.modificafrequencia();
+
+  }
+
+}
